47c506b284ec6273b67d5955ab5a509602283278
Krishna <krishnavarela.a@gmail.com>
2023-08-02 12:21:32 -0300

    feat: pagination max 12

--- _core/settings.py
+++ _core/settings.py
@@ -150,7 +150,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 30,
+    "PAGE_SIZE": 12,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
--- _core/urls.py
+++ _core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +24,6 @@ urlpatterns = [
     path('api/', include("projects.urls")),
     path('api/', include("libraries.urls")),
     path('api/', include("technologies.urls")),
-    # path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
-    # path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name="swagger"),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name="swagger"),
 ]
